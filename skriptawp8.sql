90f46bdcdce629be14932dfeafe4a3ad528f5953
Katarina Živković <kzivkovic230@gmail.com>
2025-05-09 18:07:26 +0200

    Kreiranje tablice smjer

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -2,4 +2,13 @@
 
 --drop database edunovawp8;
 
-use edunovawp8;
+--use edunovawp8;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
+
