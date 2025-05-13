5165c857d3cadc526048eb090157515cde6664fe
Katarina Živković <kzivkovic230@gmail.com>
2025-05-13 19:33:56 +0200

    2.lauf

--- trgovina.sql
+++ trgovina.sql
@@ -1,20 +1,26 @@
-﻿--create database trgovina;
---use trgovina;
+﻿use master;
+go
+drop database if exists trgovina;
+go
+create database trgovina;
+go
+use trgovina;
 
---create table racuni(
---sifra int,
---broj varchar(10),
---kupac varchar(100)
---);
 
---create table proizvodi(
---sifra int,
---naziv varchar(50),
---cijena decimal(18,2)
---);
+create table racuni(
+sifra int not null primary key identity(1,1),
+broj varchar(10) not null,
+kupac varchar(100)
+);
 
---create table stavke(
---racun int,
---proizvod int,
---kolicina decimal(5,3)
---);
+create table proizvodi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+cijena decimal(18,2) not null
+);
+
+create table stavke(
+racun int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+kolicina decimal(5,3) not null
+);
