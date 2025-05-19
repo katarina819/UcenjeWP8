c890163af61af967f4d2e4561db14f3f0f6d359e
Katarina Živković <kzivkovic230@gmail.com>
2025-05-19 19:11:33 +0200

    Rad na unosu podataka

---
+++ insert.sql
@@ -0,0 +1,25 @@
+﻿use edunovawp8;
+
+select * from smjerovi;
+--1     -> ovo je šifra koju će dodijeliti baza
+insert into smjerovi (naziv, cijena, datumpokretanja, aktivan)
+values('Web programiranje', 1200.54, '2025-05-16 17:00:01', 1);
+
+insert into smjerovi (naziv) values
+--2
+('Serviser'), 
+--3
+('Web dizajn'), 
+--4
+('Marketing');
+
+select * from grupe;
+
+insert into grupe (naziv, smjer)
+values
+--1
+('WP8', 1), 
+--2
+('WP7', 1), 
+--3
+('S1', 2);
