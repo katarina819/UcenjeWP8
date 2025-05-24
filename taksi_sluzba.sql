26154ca00c8104f021c4395082abdc161a018c63
Katarina Živković <kzivkovic230@gmail.com>
2025-05-24 17:04:43 +0200

    Kreiranje SQL skripte na osnovu ERA dijagrama

---
+++ taksi_sluzba.sql
@@ -0,0 +1,40 @@
+﻿use master;
+go
+drop database if exists taksi_sluzba;
+go 
+create database taksi_sluzba;
+go
+use taksi_sluzba;
+
+
+create table vozaci(
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar (50) not null
+);
+
+create table vozila(
+sifra int not null primary key identity(1,1),
+registracija varchar (20) not null,
+marka varchar (20) not null,
+vozac int unique references vozaci(sifra)
+);
+
+
+create table voznje(
+sifra int not null primary key identity(1,1),
+datum datetime not null,
+vrijeme time not null,
+lokacija varchar (100) not null,
+vozac int not null references vozaci(sifra)
+);
+
+create table putnici(
+sifra int not null primary key identity(1,1),
+ime varchar (50),
+prezime varchar (50),
+kontakt varchar (50) not null,
+voznja int not null references voznje(sifra),
+--voznja int unique references voznje(sifra)
+);
+
