9b3f2f56dfa011c6425d4432a847b7e41b229e09
Katarina Živković <kzivkovic230@gmail.com>
2025-05-24 18:42:28 +0200

    Kreiranje SQL skripte na osnovu ERA dijagrama

--- udrugazazivotinje.sql
+++ udrugazazivotinje.sql
@@ -1,26 +1,30 @@
-﻿--create database udruga;
---use udruga;
+﻿use master;
+go
+drop database if exists udrugazazivotinje;
+go 
+create database udrugazazivotinje;
+go
+use udrugazazivotinje;
 
---create table osobe(
---ime varchar(50),
---prezime varchar(50),
---sifra int,
---pozicija varchar(50)
---);
+create table osobe(
+sifra int not null primary key identity (1,1),
+ime varchar(50),
+prezime varchar(50),
+pozicija varchar(50)
+);
 
---drop table sticenik;
 
---create table sticenici(
---sifra int,
---ime varchar(50),
---vrsta varchar(50),
---spol varchar(50)
---);
+create table prostori(
+sifra int not null primary key identity(1,1),
+naziv varchar(50),
+vrsta varchar(50)
+);
 
---drop table prostor;
-
---create table prostori(
---sifra int,
---naziv varchar(50),
---vrsta varchar(50)
---);
+create table sticenici(
+sifra int not null primary key identity (1,1),
+ime varchar(50),
+vrsta varchar(50),
+spol varchar(50),
+osoba int not null references osobe(sifra),
+prostor int not null references prostori(sifra)
+);
